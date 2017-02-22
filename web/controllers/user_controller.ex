1740cc25eab48fe7bff6566aff84fd5a450d821d
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-22 13:20:54 +0200

    use changesets

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,6 +1,7 @@
 defmodule Kenbruen.UserController do
   use Kenbruen.Web, :controller
   import Kenbruen.UserView
+  alias Kenbruen.User
 
   def index(conn, _params) do
     users = Repo.all(Kenbruen.User)
@@ -10,5 +11,10 @@ defmodule Kenbruen.UserController do
   def show(conn, %{"id" => id}) do
     user = Repo.get(Kenbruen.User, id)
     render conn, "show.html", user: user
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
   end
 end
