4ef8e76d3d6e6ef2d724e70926c61932b561b74d
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-22 19:35:48 +0200

    hash passwords

--- web/models/user.ex
+++ web/models/user.ex
@@ -10,9 +10,29 @@ defmodule Kenbruen.User do
     timestamps
   end
 
+  #  changeset for non-sensitive information
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, ~w(name username), [])
     |> validate_length(:username, min: 1, max: 20)
   end
+
+  # changeset for sensitive information
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  def put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
+  end
+
 end
