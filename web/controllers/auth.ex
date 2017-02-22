6196cce53937df1de627d446e80f820b7b03ff97
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-22 20:37:44 +0200

    add user to the session

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -10,4 +10,13 @@ defmodule Kenbruen.Auth do
     user = user_id && repo.get(Kenbruen.User, user_id)
     assign(conn, :current_user, user)
   end
+
+  # store user ID in session
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
+
 end
