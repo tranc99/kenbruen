8c0c15fec6780db81564314eacd974b9a878c143
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-22 12:07:45 +0200

    create users

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,3 +1,12 @@
 defmodule Kenbruen.User do
-  defstruct [:id, :name, :username, :password]
+  use Kenbruen.Web, :model
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps
+  end
 end
