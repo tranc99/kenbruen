c5ffafcadedf8a6d8003e996dd6792f687b10e11
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-24 18:30:45 +0200

    set up for the test

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -15,4 +15,18 @@ defmodule Kenbruen.VideoControllerTest do
         assert conn.halted
       end)
   end
+
+  setup do
+    user = insert_user(username: "max")
+    conn = assign(conn(), :current_user, user)
+    {ok, conn: conn, user: user}
+  end
+
+  test "lists all user's videos on index", %{conn: conn, user: user} do
+    user_video = insert_video(user, title: "funny cats")
+    other_video = insert_video(insert_user(username: "other"), title: "another video")
+
+    conn = get conn, video_path(conn, :index)
+    assert html_response(conn, 200) =~ r/Listing videos/
+  end
 end
