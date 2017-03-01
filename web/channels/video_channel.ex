fda6027a374ecf3d6f631fe5178b6cdfd9e19186
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-01 18:19:23 +0200

    receive and broadcast a new annotation

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -6,6 +6,16 @@ defmodule Kenbruen.VideoChannel do
     # {:ok, assign(socket, :video_id, String.to_integer(video_id) )}
   end
 
+  def handle_in("new_annotation", params, socket) do
+    broadcast! socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    }
+
+    {:reply, :ok, socket}
+  end
+
   def handle_info(:ping, socket) do
     count = socket.assigns[:count] || 1
     push socket, "ping", %{count: count}
