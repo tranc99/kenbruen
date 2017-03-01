33d7919d725ee21c01898a5c7c24c457504eed02
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-01 20:22:34 +0200

    persist and broadcast annotations

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -14,20 +14,20 @@ defmodule Kenbruen.VideoChannel do
     changeset =
       user
       |> build_assoc(:annotations, video_id: socket.assigns.video_id)
-  end
-    broadcast! socket, "new_annotation", %{
-      user: %{username: "anon"},
-      body: params["body"],
-      at: params["at"]
-    }
+      |> Kenbruen.Annotation.changeset(params)
 
-    {:reply, :ok, socket}
+    case Repo.insert(changeset) do
+      {:ok, annotation} ->
+        broadcast! socket, "new_annotation", %{
+          id: annotation.id,
+          user: Kenbruen.UserView.render("user.json", %{user: user}),
+          body: annotation.body,
+          at: annotation.at
+        }
+        {:reply, :ok, socket}
+      {:error, changeset} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 
-  def handle_info(:ping, socket) do
-    count = socket.assigns[:count] || 1
-    push socket, "ping", %{count: count}
-
-    {:noreply, assign(socket, :count, count + 1)}
-  end
 end
