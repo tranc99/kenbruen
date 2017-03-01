c4b74f747ffc26816b6e3163ff0eeab39fcfd57a
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-02 00:10:03 +0200

    provide a last seen annotation param

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -37,6 +37,7 @@ let Video = {
     });
 
     vidChannel.on("new_annotation", (resp) => {
+      vidChannel.params.last_seen_id = resp.id;
       this.renderAnnotation(msgContainer, resp);
     });
 
@@ -44,6 +45,10 @@ let Video = {
     vidChannel.join()
       .receive("ok", resp =>  {
           console.log("joined the video channel ");
+          let ids = resp.annotations.map(ann => ann.id);
+          if(ids.length > 0) {
+            vidChannel.params.last_seen_id = Math.max(...ids);
+          }
           this.scheduleMessages(msgContainer, resp.annotations);
         })
       .receive("error", reason => console.log("join failed ", reason))
