f40465d4d72244dacf12df6460bab97b3a009a53
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-03 16:46:33 +0200

    brilliant concurrency

--- lib/kenbruen.ex
+++ lib/kenbruen.ex
@@ -14,6 +14,7 @@ defmodule Kenbruen do
       supervisor(Kenbruen.Endpoint, []),
       # Start your own worker by calling: Kenbruen.Worker.start_link(arg1, arg2, arg3)
       # worker(Kenbruen.Worker, [arg1, arg2, arg3]),
+      worker(Kenbruen.Counter, [5]),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
