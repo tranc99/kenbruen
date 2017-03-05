af9ccb7fa040584295ede1de53d7605853f6f4d0
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-05 02:42:18 +0200

    the information system for annotations

--- lib/kenbruen/info_sys.ex
+++ lib/kenbruen/info_sys.ex
@@ -1,3 +1,28 @@
 defmodule Kenbruen.InfoSys do
+  # define our backends
+  @backends [Kenbruen.InfoSys.Wolfram]
 
+  defmodule Result do
+    defstruct score: 0, text: nil, url: nil, backend: nil
+  end
+
+  def start_link(backend, query, query_ref, owner, limit) do
+    backend.start_link(query, query_ref, owner, limit)
+  end
+
+  # compute
+  def compute(query, opts \\ []) do
+    limit = opts[:limit] || 10
+    backends = opts[:backends] || @backends
+
+    backends
+    |> Enum.map(&spawn_query(&1, query, limit))
+  end
+
+  defp spawn_query(backend, query, limit) do
+    query_ref = make_ref()
+    opts = [backend, query, query_ref, self(), limit]
+    {:ok, pid} = Supervisor.start_child(Kenbruen.InfoSys.Supervisor, opts)
+    {pid, query_ref}
+  end
 end
