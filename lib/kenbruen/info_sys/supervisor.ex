74483130f9bddfbbd0d74a1aa2002714a0d9044e
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-05 02:10:50 +0200

    start OTP supervisor

--- lib/kenbruen/info_sys/supervisor.ex
+++ lib/kenbruen/info_sys/supervisor.ex
@@ -5,5 +5,12 @@ defmodule Kenbruen.InfoSys.Supervisor do
     Supervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
-  
+  def init(_opts) do
+    children = [
+      worker(Kenbruen.InfoSys, [], restart: :temporary)
+    ]
+
+    supervise children, strategy: :simple_one_for_one
+  end
+
 end
