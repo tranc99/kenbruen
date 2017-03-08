2666aa0444d6228f8e27e16b9d52d5d3094f3fe4
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-08 11:38:37 +0200

    timeout when results are late

--- lib/kenbruen/info_sys.ex
+++ lib/kenbruen/info_sys.ex
@@ -31,7 +31,11 @@ defmodule Kenbruen.InfoSys do
   end
 
   defp await_results(children, _opts) do
-    await_result(children, [], :infinity)
+    timeout = opts[:timeout] || 5000
+    timer = Process.send_after(self(), :timedout, timeout)
+    results = await_result(children, [], :infinity)
+    cleanup(timer)
+    results
   end
 
   defp await_result([head|tail], acc, timeout) do
@@ -41,14 +45,36 @@ defmodule Kenbruen.InfoSys do
     receive do
       {:results, ^query_ref, results} ->
         Process.demonitor(monitor_ref, [:flush])
-
         await_result(tail, results ++ acc, timeout)
       {:DOWN, ^monitor_ref, :process, ^pid, _reason} ->
         await_result(tail, acc, timeout)
+      :timedout ->
+        kill(pid, monitor_ref)
+        await_result(tail, acc, 0)
+    after
+      timeout ->
+        kill(pid, monitor_ref)
+        await_result(tail, acc, 0)
     end
   end
 
   defp await_result([], acc, _) do
     acc
+  end
+
+  defp kill(pid, ref) do
+    Process.demonitor(ref, [:flush])
+    Process.exit(pid, :kill)
+  end
+
+  defp cleanup(timer) do
+    :erlang.cancel_timer(timer)
+    receive do
+      :timedout -> :ok
+    end
+
+    after
+      0 -> :ok
+    end
   end
 end
